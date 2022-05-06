24ed24479c608a16ce9069e0cb020ba085730a91
blaxxun <blaxxun@blaxxun.blaxxun>
2022-05-07 00:00:21 +0200

    change server config to yaml

---
+++ Definition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using YamlDotNet.Serialization;
+
+namespace FastLink;
+
+public class Definition
+{
+	public string serverName = null!;
+
+	public ushort port { get; set; }
+
+	public string address { get; set; } = null!;
+	
+	public string password { get; set; } = "";
+
+	public static IEnumerable<Definition> Parse(string yaml) => new DeserializerBuilder().IgnoreFields().Build().Deserialize<Dictionary<string, Definition>>(yaml).Select(kv =>
+	{
+		Definition def = kv.Value;
+		def.serverName = kv.Key;
+		return def;
+	});
+
+	public override string ToString() => $"Server(name={serverName},address={address},port={port})";
+}
